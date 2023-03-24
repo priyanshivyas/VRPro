5981cd73312180d16615f154251e5a88d0639b86
Tushar Agarwal <tushar.agarwal0901@gmail.com>
2023-03-24 22:38:45 +0530

    Debugging phase 2

--- main.py
+++ main.py
@@ -1,87 +1,42 @@
 import argparse
 import numpy as np
 import pandas as pd
-from vrp_solver import solve_vrp, VRP
-from vrp_utils import calculate_distance, find_nearest_customer
-def parse_args():
+from vrp_solver import VRP
 
-    parser = argparse.ArgumentParser(description='Vehicle Routing Problem with Pickup and Delivery')
+def main():
+    parser = argparse.ArgumentParser(description='Solve Vehicle Routing Problem')
+    # parser.add_argument('--num_customers', type=int, required=True, help='Number of customers')
+    # parser.add_argument('--customer_locations', type=str, required=True, help='CSV file containing customer locations')
+    # parser.add_argument('--num_vehicles', type=int, required=True, help='Number of vehicles')
 
-    parser.add_argument('--num_customers', type=int, default=5,
+    parser.add_argument('--num_customers', type=int, default=14,
                         help='number of customers (default: 5)')
     parser.add_argument('--customer_locations', type=str, default='customers.csv',
                         help='path to CSV file containing customer locations')
     parser.add_argument('--num_vehicles', type=int, default=3,
                     help='number of vehicles (default: 3)')
-
-    return parser.parse_args()
-def load_data(input_file):
-    with open(input_file, 'r') as f:
-        data = np.genfromtxt(f, delimiter=',', dtype=float, skip_header=1)
-    return data
-# def main():
-#     args = parse_args()
-#     input_data = load_data(args.input_file)
-#     num_customers = input_data.shape[0]
-#     num_vehicles = args.num_vehicles
-    
-#     # Call the solver function
-#     # routes, num_used_vehicles = solve_vrp(num_customers, input_data, num_vehicles)
-#     routes, num_used_vehicles = solve_vrp(num_customers, input_data)
-    
-#     # Output the results
-#     print(f"Optimized routes: {routes}")
-#     print(f"Number of vehicles used: {num_used_vehicles}")
-# if __name__ == '__main__':
-#     main()
-
-# import argparse
-# import numpy as np
-# from vrp_solver import solve_vrp
-
-# def main():
-#     # Parse command-line arguments
-#     parser = argparse.ArgumentParser(description='Solve the Vehicle Routing Problem using the nearest neighbor algorithm')
-#     parser.add_argument('--num_customers', type=int, help='Number of customers', required=True)
-#     parser.add_argument('--customer_locations', type=str, help='List of customer locations', required=True)
-#     parser.add_argument('--num_vehicles', type=int, help='Number of vehicles', required=True)
-#     args = parser.parse_args()
-
-#     # Load input data
-#     customer_locations = np.fromstring(args.customer_locations, sep=' ').reshape(args.num_customers, 2)
-
-#     # Solve the problem
-#     solution = solve_vrp(customer_locations, args.num_vehicles)
-
-#     # Print solution
-#     print('Vehicle routes:', solution)
-
-def main():
-    # Parse command-line arguments
-    parser = argparse.ArgumentParser(description='Solve the Vehicle Routing Problem using the nearest neighbor algorithm')
-    parser.add_argument('--num_customers', type=int, help='Number of customers', required=True)
-    parser.add_argument('--customer_locations', type=str, help='List of customer locations', required=True)
-    parser.add_argument('--num_vehicles', type=int, help='Number of vehicles', required=True)
     args = parser.parse_args()
 
-    # read customer locations from CSV file
-    customer_locations = load_data(args.customer_locations)
+    # Load customer locations from CSV file
+    customer_locations = np.loadtxt(args.customer_locations, delimiter=',', skiprows=1, usecols=(0,1))
 
-    # reshape customer_locations to be num_customers x 2
+    # Check if number of customers matches the shape of customer_locations array
+    if args.num_customers != customer_locations.shape[0]:
+        print(f"Error: Number of customers in input ({args.num_customers}) does not match the number of rows in {args.customer_locations} ({customer_locations.shape[0]}).")
+        return
+
+    # Reshape customer_locations array to match VRP solver input format
     customer_locations = customer_locations.reshape(args.num_customers, 2)
 
-    # initialize VRP instance
-    vrp = vrp(num_customers=args.num_customers, customer_locations=customer_locations, num_vehicles=args.num_vehicles)
-
-    # solve VRP instance
+    # Solve VRP problem
+    vrp = VRP(num_customers=args.num_customers, customer_locations=customer_locations, num_vehicles=args.num_vehicles)
     solution = vrp.solve()
-
-    # print solution
-    print(solution)
-
-    print('Input data:', customer_locations, args.num_vehicles)
-    solution = solve_vrp(customer_locations, args.num_vehicles)
-    print('Output solution:', solution)
+    
+    # Print solution
+    print("Total distance: ", solution['total_distance'])
+    print("Routes:")
+    for i, route in enumerate(solution['routes']):
+        print(f"Vehicle {i}: {route}")
 
 if __name__ == '__main__':
     main()
--- vrp_solver.py
+++ vrp_solver.py
@@ -1,89 +1,48 @@
-from vrp_utils import calculate_distance, find_nearest_customer
 import numpy as np
-import argparse
+from scipy.spatial.distance import pdist, squareform
 
-# def solve_vrp(customer_locations, num_vehicles):
-#     """
-#     Solve the Vehicle Routing Problem using the nearest neighbor algorithm
-#     """
-#     num_customers = len(customer_locations) - 1 # Substract 1 from the length.
-#     visited_customers = set()
-#     vehicle_routes = [[] for _ in range(num_vehicles)]
-
-#     # Start at the first customer location for each vehicle
-#     current_location = [customer_locations[i] for i in range(num_vehicles)]
-
-#     # Loop through all customers and add to the nearest vehicle
-#     while len(visited_customers) < num_customers:
-#         for i, location in enumerate(current_location):
-#             if len(visited_customers) == num_customers:
-#                 break
-#             nearest_customer = find_nearest_customer(location, customer_locations, visited_customers)
-#             vehicle_routes[i].append(nearest_customer)
-#             visited_customers.add(nearest_customer)
-
-#             # Update current location to nearest customer
-#             current_location[i] = customer_locations[nearest_customer]
-
-#     return vehicle_routes
 
 class VRP:
     def __init__(self, num_customers, customer_locations, num_vehicles):
         self.num_customers = num_customers
         self.customer_locations = customer_locations
         self.num_vehicles = num_vehicles
+        self.distance_matrix = squareform(pdist(self.customer_locations))
 
+    def solve(self):
+        routes = []
+        route_distances = []
 
-def solve_vrp(customer_locations, num_vehicles):
-    # Initialize solution
-    solution = [[] for i in range(num_vehicles)]
-    unvisited_customers = set(range(len(customer_locations)))
-    visited_customers = set()
+        vehicle_capacities = np.ones(self.num_vehicles) * 10  # default capacity of 10
 
-    # Iterate over vehicles
-    for vehicle in range(num_vehicles):
-        # Select random customer as starting point
-        current_customer = np.random.choice(list(unvisited_customers))
-        while True:
-            # Remove current customer from unvisited set and add to visited set
-            unvisited_customers.remove(current_customer)
-            visited_customers.add(current_customer)
+        for v in range(self.num_vehicles):
+            route = []
+            route_distance = 0
 
-            # Add current customer to current vehicle route
-            solution[vehicle].append(current_customer)
+            curr_customer = 0  # start at the depot
 
-            # Find nearest unvisited customer to current customer
-            nearest_customer = find_nearest_customer(customer_locations[current_customer],
-                                                      customer_locations,
-                                                      visited_customers)
-            if nearest_customer is None:
-                # No more unvisited customers, end route
-                break
+            while True:
+                # Find the nearest customer
+                nearest_customer = np.argmin(self.distance_matrix[curr_customer][1:] + np.where(vehicle_capacities[v] < 1e-8, np.inf, 0))
 
-            current_customer = nearest_customer
+                # If no customers are reachable, we're done with this route
+                if np.isinf(self.distance_matrix[curr_customer][nearest_customer + 1]):
+                    break
 
-        print('Route {} :'.format(vehicle), solution[vehicle])
+                # Add the customer to the route
+                route.append(nearest_customer)
+                route_distance += self.distance_matrix[curr_customer][nearest_customer + 1]
+                vehicle_capacities[v] -= 1
 
-    return solution
+                # Move to the new current customer
+                curr_customer = nearest_customer + 1
 
+            # Add the depot to the end of the route
+            route.append(0)
+            route_distance += self.distance_matrix[curr_customer][0]
 
-if __name__ == '__main__':
-    # Parse command-line arguments
-    parser = argparse.ArgumentParser(description='Solve the Vehicle Routing Problem using the nearest neighbor algorithm')
-    parser.add_argument('--num_customers', type=int, help='Number of customers', required=True)
-    parser.add_argument('--customer_locations', type=str, help='List of customer locations', required=True)
-    parser.add_argument('--num_vehicles', type=int, help='Number of vehicles', required=True)
-    args = parser.parse_args()
+            # Add the completed route and its distance to the overall solution
+            routes.append(route)
+            route_distances.append(route_distance)
 
-    # Load input data
-    customer_locations = np.fromstring(args.customer_locations, sep=' ').reshape(args.num_customers, 2)
-
-    # Solve the problem
-    solution = solve_vrp(customer_locations, args.num_vehicles)
-
-    # Print solution
-    print('Vehicle routes:', solution)
-    print('Number of vehicles:', len(solution))
-
-
-
+        return routes, route_distances
